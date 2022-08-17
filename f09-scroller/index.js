85af1957efc9f4d8308edc770199fe454806bd01
andrzejszl <andrzej.sz123@gmail.com>
2022-08-17 12:45:42 +0200

    Add support for keyboard

--- f09-scroller/index.js
+++ f09-scroller/index.js
@@ -3,4 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
     document.addEventListener('wheel', (event) => scroller.listenScroll(event));
     document.addEventListener('swipeUp', () => scroller.scroll(1));
     document.addEventListener('swipeDown', () => scroller.scroll(-1));
-})
+    document.addEventListener('keydown', (event) => {
+        switch (event.code) {
+            case "ArrowDown":
+                return scroller.scroll(1);
+                break;
+            case "ArrowUp":
+                return scroller.scroll(-1);
+                break;
+            default:
+                return;
+        }
+    })
+});
