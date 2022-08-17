7fb2c3821351aa015d0a7bfab56804df6f848aa8
andrzejszl <andrzej.sz123@gmail.com>
2022-08-17 11:14:05 +0200

    add navigation to the scroller

---
+++ f09-scroller/Scroller.js
@@ -0,0 +1,87 @@
+class Scroller {
+    constructor(rootSelector) {
+        const rootElement = document.querySelector(rootSelector);
+        this.sections = document.querySelectorAll('section');
+        const sectionsArr = [...this.sections];
+        const currentSectionIndex = sectionsArr.findIndex(this.isScrolledIntoView);
+        this.currentSectionIndex = Math.max(currentSectionIndex, 0);
+        this.isThrottled = false;
+
+        this.drawNavigation();
+    }
+
+    isScrolledIntoView(el) {
+        const rect = el.getBoundingClientRect()
+        const elemTop = rect.top;
+        const elemBottom = rect.bottom;
+        const isVisible = (elemTop >= 0) && (elemBottom <= Math.floor(window.innerHeight));
+
+    }
+
+    listenScroll = (e) => {
+        if (this.isThrottled) return;
+        this.isThrottled = true;
+
+        setTimeout(() => {
+            this.isThrottled = false
+        }, 500);
+
+        const direction = e.wheelDelta < 0 ? 1 : -1;
+        this.scroll(direction);
+    }
+    scroll = (direction) => {
+        if (direction === 1) {
+            const isLastSection = this.currentSectionIndex === this.sections.length - 1;
+            if (isLastSection) return;
+        } else if (direction === -1) {
+            const firstSection = this.currentSectionIndex === 0;
+            if (firstSection) return
+        }
+
+        this.currentSectionIndex = this.currentSectionIndex + direction;
+        this.scrollToCurrentSection();
+    }
+    scrollToCurrentSection = () => {
+        this.selectActiveNavItem();
+        this.sections[this.currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        })
+    }
+
+    drawNavigation = () => {
+        this.navigationContainer = document.createElement('aside');
+        this.navigationContainer.classList.add('scroller__navigation');
+        const list = document.createElement('ul');
+
+        this.sections.forEach((section, index) => {
+            const listItem = document.createElement('li');
+
+            listItem.addEventListener('click', () => {
+                this.currentSectionIndex = index;
+
+                this.scrollToCurrentSection();
+            })
+
+            list.appendChild(listItem);
+        })
+        this.navigationContainer.appendChild(list);
+
+        document.body.appendChild(this.navigationContainer);
+        this.selectActiveNavItem();
+    }
+
+    selectActiveNavItem = () => {
+        if (this.navigationContainer) {
+            const navigationItems = this.navigationContainer.querySelectorAll('li');
+
+            navigationItems.forEach((item, index) => {
+                if (index === this.currentSectionIndex) {
+                    item.classList.add('active');
+                } else {
+                    item.classList.remove('active');
+                }
+            })
+        }
+    }
+}
