7d013c5902230739f4ef491c4384aa4c370d7218
andrzejszl <andrzej.sz123@gmail.com>
2022-08-18 13:38:14 +0200

    Create AnimBg class

--- f10-bg-animation/index.js
+++ f10-bg-animation/index.js
@@ -1,25 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const elements = document.querySelectorAll('.anim-bg');
-
-    document.addEventListener('mousemove', function (event) {
-        const {
-            clientX,
-            clientY
-        } = event;
-
-        const centerX = window.innerWidth / 2;
-        const centerY = window.innerHeight / 2;
+    const animBg = new AnimBg('.anim-bg')
 
 
-        elements.forEach(element => {
-            const ratioX = -element.getAttribute('ratioX');
-            const ratioY = -element.getAttribute('ratioY');
-
-            const moveX = (clientX - centerX) * ratioX;
-            const moveY = (clientY - centerY) * ratioY;
-
-
-            element.style.transform = `translate(${moveX}px, ${moveY}px)`
-        })
-    })
+    document.addEventListener('mousemove', (event) => animBg.listenCursorMove(event))
 })
