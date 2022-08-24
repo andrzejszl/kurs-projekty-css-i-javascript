0c68beb7211ba122b2c1347080622f1881e3318f
andrzejszl <andrzej.sz123@gmail.com>
2022-08-24 17:29:51 +0200

    final commit

--- f16-fetch-galeria-zdjec/app/index.js
+++ f16-fetch-galeria-zdjec/app/index.js
@@ -1,5 +1,17 @@
 import '../sass/style.scss';
 
+
+function showLoading() {
+    const spinnerEl = document.querySelector('.spinner');
+    spinnerEl.classList.add('spinner--visible');
+}
+
+function hideLoading() {
+    const spinnerEl = document.querySelector('.spinner');
+    spinnerEl.classList.remove('spinner--visible');
+}
+
+
 function listBreeds() {
     const apiUrl = "https://dog.ceo/api";
     return fetch(`${apiUrl}/breeds/list/all`)
@@ -26,14 +38,12 @@ function showAllBreeds() {
         .then(breeds => {
             for (const breed in breeds) {
                 if (breeds[breed].length === 0) {
-                    // console.log(breed);
+                    addBreed(breed);
                 } else {
-
                     breeds[breed].forEach(subBreed => {
-                        console.log(breed + "/" + subBreed)
+                        addBreed(breed, subBreed)
+                        // console.log(breed + "/" + subBreed)
                     })
-
-
 
                     // for (const subBreed of breeds[breed]) {
                     //     console.log(breed + "/" + subBreed);
@@ -43,16 +53,58 @@ function showAllBreeds() {
         })
 }
 
-function init() {
+function showImageWhenReady(image) {
+    const backgroundEl = document.querySelector('.featured-dog__background');
+    const imgEl = document.querySelector('.featured-dog img');
+    imgEl.setAttribute('src', image);
+    backgroundEl.style.background = `url("${image}")`;
+    hideLoading();
+}
 
+function addBreed(breed, subBreed) {
+    const tilesEl = document.querySelector('.tiles');
+    let name;
+    let type;
+
+    if (typeof subBreed === 'undefined') {
+        name = breed;
+        type = breed;
+    } else {
+        name = `${breed} ${subBreed}`;
+        type = `${breed}/${subBreed}`;
+    }
+
+    const tile = document.createElement('div');
+    tile.classList.add('tiles__tile');
+
+    const backgroundEl = document.querySelector('.featured-dog__background');
+    const imgEl = document.querySelector('.featured-dog img');
+    const tileContent = document.createElement('div');
+    tileContent.classList.add('tiles__tile-content');
+    tileContent.innerText = name;
+    tileContent.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+        showLoading();
+        getRandomImageByBreed(type)
+            .then(showImageWhenReady)
+    })
+
+
+
+    tile.appendChild(tileContent);
+    tilesEl.appendChild(tile)
+
+}
+
+function init() {
+    showLoading();
     const imgEl = document.querySelector('.featured-dog img');
     const backgroundEl = document.querySelector('.featured-dog__background');
     getRandomImage()
-        .then(src => {
-            imgEl.setAttribute('src', src);
-            backgroundEl.style.background = `url("${src}")`;
-        })
-
+        .then(showImageWhenReady)
     showAllBreeds();
 }
 
