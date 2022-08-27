bb68aff7603df3c71c2a71fb9e1c47addc243da3
andrzejszl <andrzej.sz123@gmail.com>
2022-08-27 18:24:15 +0200

    add fps limit

--- f23-animacje-css-i-javascript/spinner/spinner.js
+++ f23-animacje-css-i-javascript/spinner/spinner.js
@@ -10,3 +10,36 @@ const rotate = () => {
 }
 
 setInterval(rotate, fps);
+
+
+const spinnerRAF = document.querySelector('.v3');
+
+let degRAF = 0;
+const degChangeRAF = 6; //360 stopni / 60 klatek/sekunde
+
+const rotateRAF = () => {
+    degRAF += degChangeRAF;
+    spinnerRAF.style.transform = `rotate(${degRAF}deg)`;
+    requestAnimationFrame(rotateRAF)
+}
+
+rotateRAF()
+
+
+const spinnerRAF2 = document.querySelector('.v4');
+
+let degRAF2 = 0;
+const degChangeRAF2 = 6; //360 stopni / 60 klatek/sekunde
+let time = performance.now();
+console.log(time);
+
+const rotateRAF2 = (currentT) => {
+    if (currentT - time > 16) {
+        time = currentT;
+        degRAF2 += degChangeRAF2;
+        spinnerRAF2.style.transform = `rotate(${degRAF2}deg)`;
+    }
+    requestAnimationFrame(rotateRAF2)
+}
+
+rotateRAF2()
