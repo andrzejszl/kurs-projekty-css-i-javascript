3b905ded64106140d02d06749e227047374be657
andrzejszl <andrzej.sz123@gmail.com>
2022-08-27 08:20:18 +0200

    final commit

--- f20-projekt5-responsywnosc/webpack.config.js
+++ f20-projekt5-responsywnosc/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   devServer: {
     static: [{
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, 'assets'),
       watch: true,
     }],
     port: 3500,
